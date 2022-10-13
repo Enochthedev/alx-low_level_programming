c99ad0af9659cb39ac52395f3bb0c42f70f0ecc8
Enochthedev <enochjesse884@gmail.com>
2022-10-13 12:51:58 -0700

    last lap

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-/*Write a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line. 
- * must end in 0*/
-
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
