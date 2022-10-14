7a144276baa90101fe66a9683126e5ec2c4cb3c9
Enochthedev <enochjesse884@gmail.com>
2022-10-14 14:20:47 -0700

    correction

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,22 +2,21 @@
 
 /**
  * main - prints the alphabet in lowercase,
- * except q and e 
- * followed by a new line
- * Return: Always 0 (Success)
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
  */
 int main(void)
 {
 	char ch = 'a';
-	
-	while (ch <='z')
+
+	while (ch <= 'z')
 	{
-		if (ch !='e' && ch !='q')
+		if (ch != 'e' && ch != 'q')
 		{
 			putchar(ch);
 		}
 		ch++;
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
