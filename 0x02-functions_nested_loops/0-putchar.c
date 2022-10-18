e32fcba237d8d00a0137e8f84ca1ae738786e1a3
Enochthedev <enochjesse884@gmail.com>
2022-10-18 10:51:28 -0700

    Test no 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * printf - writes the character _putchar to stdout
+ *
+ * Return: On success 0
+ */
+main(void)
+{
+        char *pt = "_putchar";
+                printf(pt,'%c');
+                return(0);
+}
