45acee99455297963b14932a2e6e1b1ab2e4cf31
Toluwani omosebi <enochjesse884@outlook.com>
2023-03-22 14:48:47 +0100

    changed syntax

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,22 +1,31 @@
 #!/usr/bin/python3
 """5-island_perimeter module"""
 
-
 def island_perimeter(grid):
-    """Returns the perimeter of the island described in grid"""
-    c = 0
-    length = len(grid) - 1
-    width = len(grid[0]) - 1
+    """
+    Returns the perimeter of the island described in grid.
+    
+    Args:
+        grid (list of list of int): A list of lists representing the grid.
+                                   0 represents a water zone.
+                                   1 represents a land zone.
+    
+    Returns:
+        int: The perimeter of the island in the grid.
+    """
+    perimeter = 0
+    grid_height = len(grid) - 1
+    grid_width = len(grid[0]) - 1
 
-    for i, r in enumerate(grid):
-        for j, n in enumerate(r):
-            if n == 1:
-                if i == 0 or grid[i - 1][j] != 1:
-                    c += 1
-                if j == 0 or grid[i][j - 1] != 1:
-                    c += 1
-                if j == width or grid[i][j + 1] != 1:
-                    c += 1
-                if i == length or grid[i + 1][j] != 1:
-                    c += 1
-    return c
+    for row_idx, row in enumerate(grid):
+        for col_idx, cell in enumerate(row):
+            if cell == 1:
+                if row_idx == 0 or grid[row_idx - 1][col_idx] != 1:
+                    perimeter += 1
+                if col_idx == 0 or grid[row_idx][col_idx - 1] != 1:
+                    perimeter += 1
+                if col_idx == grid_width or grid[row_idx][col_idx + 1] != 1:
+                    perimeter += 1
+                if row_idx == grid_height or grid[row_idx + 1][col_idx] != 1:
+                    perimeter += 1
+    return perimeter
